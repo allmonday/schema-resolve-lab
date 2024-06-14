e388265970656618c39fa37d748c159c03ae874f
min.tang <min.tang@ubisoft.com>
2024-06-14 14:05:46 +0800

    add meta check

--- research/get-meta.ts
+++ research/get-meta.ts
@@ -1,8 +1,11 @@
 import "reflect-metadata"
 import { Expose, plainToInstance, instanceToPlain, Type } from 'class-transformer';
+import {IsInt, IsString, Max, Min, isNumber, validate} from 'class-validator'
 
+// class-transform 无法确保返回类型符合ts 类型标注， 所以需要配合 class-validator 来做检验。
 
 class Info {
+    @IsString()
     hobby!: string
 
     resolve_hobby(): string {
@@ -11,7 +14,12 @@ class Info {
 }
 
 class User {
+    @IsInt()
     id!: number;
+
+    @IsString()
+    content!: string
+
     @Type(() => Info)
     info!: Info
 }
@@ -25,3 +33,11 @@ const getType = (obj: object, field: string) => {
 
 console.log(getType(u, 'id')) // undefined
 console.log(getType(u, 'info'))
+
+const uu = plainToInstance(User, {id: 12, content: 111, info: {hobby: 'writing'}}, {})
+console.log(uu)
+validate(uu).then(errors => {
+    console.log(errors)
+})
+
+console.log(typeof uu)
